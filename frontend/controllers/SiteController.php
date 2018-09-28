84ebc374bfcfdd77b4f946321f6241322172e838
heckslay <lashaa.lomidze@gmail.com>
2018-09-28 13:26:34 +0400

    Use landing layout when rendering site/index page

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -59,7 +59,8 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $this->layout = 'landing';
+        return $this->renderContent('kgkyuti876tugjhgfjfg');
     }
 
     /**
